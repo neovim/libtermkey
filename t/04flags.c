d8232917b077a0967f2e03e1c8759b62b65c35e6
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2012-01-18 13:39:50 +0000

    Added termkey_push_bytes(), a new API for providing input bytes

--- t/04flags.c
+++ t/04flags.c
@@ -4,22 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-  int        fd[2];
   TermKey   *tk;
   TermKeyKey key;
 
   plan_tests(8);
 
-  pipe(fd);
-
   /* Sanitise this just in case */
   putenv("TERM=vt100");
 
-  tk = termkey_new(fd[0], TERMKEY_FLAG_NOTERMIOS);
+  tk = termkey_new(0, TERMKEY_FLAG_NOTERMIOS);
 
-  write(fd[1], " ", 1);
+  termkey_push_bytes(tk, " ", 1);
 
-  is_int(termkey_waitkey(tk, &key), TERMKEY_RES_KEY, "waitkey yields RES_KEY after space");
+  is_int(termkey_getkey(tk, &key), TERMKEY_RES_KEY, "getkey yields RES_KEY after space");
 
   is_int(key.type,        TERMKEY_TYPE_UNICODE, "key.type after space");
   is_int(key.code.number, ' ',                  "key.code.number after space");
@@ -27,9 +24,9 @@ int main(int argc, char *argv[])
 
   termkey_set_flags(tk, TERMKEY_FLAG_SPACESYMBOL);
 
-  write(fd[1], " ", 1);
+  termkey_push_bytes(tk, " ", 1);
 
-  is_int(termkey_waitkey(tk, &key), TERMKEY_RES_KEY, "waitkey yields RES_KEY after space");
+  is_int(termkey_getkey(tk, &key), TERMKEY_RES_KEY, "getkey yields RES_KEY after space");
 
   is_int(key.type,        TERMKEY_TYPE_KEYSYM, "key.type after space with FLAG_SPACESYMBOL");
   is_int(key.code.number, TERMKEY_SYM_SPACE,   "key.code.sym after space with FLAG_SPACESYMBOL");
