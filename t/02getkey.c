a0c342d74973b3cff7150ead8e4b5c4751728b71
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2012-01-18 10:04:15 +0000

    Provide new API function to determine how much buffer space remains in the instance

--- t/02getkey.c
+++ t/02getkey.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
   TermKey   *tk;
   TermKeyKey key;
 
-  plan_tests(19);
+  plan_tests(29);
 
   pipe(fd);
 
@@ -16,6 +16,8 @@ int main(int argc, char *argv[])
   putenv("TERM=vt100");
 
   tk = termkey_new(fd[0], TERMKEY_FLAG_NOTERMIOS);
+
+  is_int(termkey_get_buffer_remaining(tk), 256, "buffer free initially 256");
 
   is_int(termkey_getkey(tk, &key), TERMKEY_RES_NONE, "getkey yields RES_NONE when empty");
 
@@ -25,12 +27,16 @@ int main(int argc, char *argv[])
 
   is_int(termkey_advisereadable(tk), TERMKEY_RES_AGAIN, "advisereadable yields RES_AGAIN after h");
 
+  is_int(termkey_get_buffer_remaining(tk), 255, "buffer free 255 after advisereadable");
+
   is_int(termkey_getkey(tk, &key), TERMKEY_RES_KEY, "getkey yields RES_KEY after h");
 
   is_int(key.type,        TERMKEY_TYPE_UNICODE, "key.type after h");
   is_int(key.code.number, 'h',                  "key.code.number after h");
   is_int(key.modifiers,   0,                    "key.modifiers after h");
   is_str(key.utf8,        "h",                  "key.utf8 after h");
+
+  is_int(termkey_get_buffer_remaining(tk), 256, "buffer free 256 after getkey");
 
   is_int(termkey_getkey(tk, &key), TERMKEY_RES_NONE, "getkey yields RES_NONE a second time");
 
@@ -53,6 +59,24 @@ int main(int argc, char *argv[])
   is_int(key.type,        TERMKEY_TYPE_KEYSYM,  "key.type after Up");
   is_int(key.code.sym,    TERMKEY_SYM_UP,       "key.code.sym after Up");
   is_int(key.modifiers,   0,                    "key.modifiers after Up");
+
+  write(fd[1], "\eO", 2);
+  termkey_advisereadable(tk);
+
+  is_int(termkey_get_buffer_remaining(tk), 254, "buffer free 254 after partial write");
+
+  is_int(termkey_getkey(tk, &key), TERMKEY_RES_AGAIN, "getkey yields RES_AGAIN after partial write");
+
+  write(fd[1], "C", 1);
+  termkey_advisereadable(tk);
+
+  is_int(termkey_getkey(tk, &key), TERMKEY_RES_KEY, "getkey yields RES_KEY after Right completion");
+
+  is_int(key.type,        TERMKEY_TYPE_KEYSYM,  "key.type after Right");
+  is_int(key.code.sym,    TERMKEY_SYM_RIGHT,    "key.code.sym after Right");
+  is_int(key.modifiers,   0,                    "key.modifiers after Right");
+
+  is_int(termkey_get_buffer_remaining(tk), 256, "buffer free 256 after completion");
 
   termkey_destroy(tk);
 
