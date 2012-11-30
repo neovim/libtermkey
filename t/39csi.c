dd711277fd174b9d62955b104316f7765e50e2ea
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2012-11-30 14:37:28 +0000

    Unit test that CSI initial command also works

--- t/39csi.c
+++ t/39csi.c
@@ -9,13 +9,13 @@ int main(int argc, char *argv[])
   size_t     nargs = 16;
   unsigned long command;
 
-  plan_tests(7);
+  plan_tests(11);
 
   tk = termkey_new_abstract("vt100", 0);
 
   termkey_push_bytes(tk, "\e[5;25v", 7);
 
-  is_int(termkey_getkey(tk, &key), TERMKEY_RES_KEY, "getkey yields RES_KEY for position report");
+  is_int(termkey_getkey(tk, &key), TERMKEY_RES_KEY, "getkey yields RES_KEY for CSI v");
 
   is_int(key.type, TERMKEY_TYPE_UNKNOWN_CSI, "key.type for unknown CSI");
 
@@ -25,6 +25,13 @@ int main(int argc, char *argv[])
   is_int(args[0],   5, "args[0] for unknown CSI");
   is_int(args[1],  25, "args[1] for unknown CSI");
   is_int(command, 'v', "command for unknown CSI");
+
+  termkey_push_bytes(tk, "\e[?w", 4);
+
+  is_int(termkey_getkey(tk, &key), TERMKEY_RES_KEY, "getkey yields RES_KEY for CSI ? w");
+  is_int(key.type, TERMKEY_TYPE_UNKNOWN_CSI, "key.type for unknown CSI");
+  is_int(termkey_interpret_csi(tk, &key, args, &nargs, &command), TERMKEY_RES_KEY, "interpret_csi yields RES_KEY");
+  is_int(command, '?' << 8 | 'w', "command for unknown CSI");
 
   termkey_destroy(tk);
 
